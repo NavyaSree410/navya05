7ebefea09ab3b945ed2065f23ea18303492073cf
Navya Sree <navyasree9490@gmail.com>
2026-04-26 18:41:11 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 public class simple {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("presidency 1");
+        System.out.println("presidency");
     }
 }
